108ee07a488f9c65e4804c5bcefa9647be768b6a
awaisajaz1 <awaisajaz1@gmail.com>
2025-12-02 08:12:49 +0000

    product materialized view

--- transformations/medallion_pipeline.py
+++ transformations/medallion_pipeline.py
@@ -13,3 +13,14 @@ def bronze_stores():
         .withColumn('read_ts', current_timestamp())
     )
 
+
+@dp.materialized_view(
+    comment='Product Bronze Table',
+    name='sdp.bronze.products'
+)
+def bronze_products():
+    return(
+        spark.read
+        .table('sdp.source.products')
+        .withColumn('read_ts', current_timestamp())
+    )
