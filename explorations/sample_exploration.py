604a4fc762ae4c685d0fcdb5bb7a8f219b69228e
awaisajaz1 <awaisajaz1@gmail.com>
2025-12-03 06:02:08 +0000

    pricing is beuing added to product source table

--- explorations/sample_exploration.py
+++ explorations/sample_exploration.py
@@ -16,4 +16,4 @@ import sys
 
 # !!! Before performing any data analysis, make sure to run the pipeline to materialize the sample datasets. The tables referenced in this notebook depend on that step.
 
-display(spark.sql("SELECT * FROM sdp.bronze.stores"))
+display(spark.sql("SELECT * FROM sdp.source.products"))
